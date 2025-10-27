fde6c85c69d82a671d6cb2f921ded9b412ce86ab
anirudh-challa <anirudh.challa108@gmail.com>
2025-10-26 19:22:52 -0700

    made changes

---
+++ script.js
@@ -0,0 +1,10 @@
+// Add JavaScript for interactivity (e.g., smooth scroll)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
